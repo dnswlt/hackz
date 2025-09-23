94a310e3b326a68236e4e1a7e763b0341dabc472
Dennis Walter <dennis.walter@gmail.com>
2025-09-23 07:28:11 +0200

    Process multiple files.

--- planr/cmd/planr/main.go
+++ planr/cmd/planr/main.go
@@ -14,22 +14,34 @@ import (
 )
 
 func main() {
-	planFile := flag.String("plan", "plan.yml", "Path to the migration plan YAML file")
+	printYaml := flag.Bool("print-yaml", true, "If set, prints the merged YAML to stdout.")
 	flag.Parse()
 
-	yamlFile, err := os.ReadFile(*planFile)
-	if err != nil {
-		log.Fatalf("Error reading YAML file: %v", err)
+	if len(flag.Args()) == 0 {
+		log.Fatal("No YAML files specified.")
 	}
 
 	var plan planpb.Plan
 
-	err = yaml.Unmarshal(yamlFile, &plan)
-	if err != nil {
-		log.Fatalf("Error unmarshaling YAML: %v", err)
+	for _, planFile := range flag.Args() {
+		yamlFile, err := os.ReadFile(planFile)
+		if err != nil {
+			log.Fatalf("Error reading YAML file: %v", err)
+		}
+
+		var p planpb.Plan
+		err = yaml.Unmarshal(yamlFile, &p)
+		if err != nil {
+			log.Fatalf("Error unmarshaling YAML: %v", err)
+		}
+
+		plan.Applications = append(plan.Applications, p.Applications...)
+		plan.Datastores = append(plan.Datastores, p.Datastores...)
+		plan.Interfaces = append(plan.Interfaces, p.Interfaces...)
+		plan.Releases = append(plan.Releases, p.Releases...)
 	}
 
-	fmt.Printf("Read %d applications from %s\n", len(plan.GetApplications()), *planFile)
+	fmt.Printf("Read %d applications from %d files\n", len(plan.GetApplications()), len(flag.Args()))
 
 	if err := planr.ValidatePlan(&plan); err != nil {
 		log.Fatalf("Plan validation failed: %v", err)
@@ -37,9 +49,11 @@ func main() {
 		log.Println("Plan validated successfully.")
 	}
 
-	bs, err := yaml.Marshal(&plan)
-	if err != nil {
-		log.Fatalf("Error marshaling YAML: %v", err)
+	if *printYaml {
+		bs, err := yaml.Marshal(&plan)
+		if err != nil {
+			log.Fatalf("Error marshaling YAML: %v", err)
+		}
+		fmt.Println(string(bs))
 	}
-	fmt.Println(string(bs))
 }
